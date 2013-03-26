4b0ee9c1ae3fa46d2d55a2e4af5631342fa8f941
kindofwhat <kindofwhat@gmail.com>
2013-03-26 08:24:46 +0100

    some changes

---
+++ vaadinbuilder-module/src/main/groovy/org/groovyvaadin/factories/DynamicComponentFactory.groovy
@@ -0,0 +1,19 @@
+package org.groovyvaadin.factories
+
+import com.vaadin.ui.Component
+
+abstract class DynamicComponentFactory extends AbstractVaadinFactory {
+
+    @Override
+    public void setChild( FactoryBuilderSupport builder, Object parent, Object child )  {
+        parent.addComponent child
+    }
+
+
+    @Override
+	public void setParent(FactoryBuilderSupport builder, Object parent,Object child) {
+		//do nothing ?
+	}
+
+
+}
